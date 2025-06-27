399361265c123a04dec21ecd7c40fbd04e606507
Sahil Raturi <sahilraturi687@gmail.com>
2025-06-27 23:53:50 +0530

    schema

--- models/hospital management/patient.models.js
+++ models/hospital management/patient.models.js
@@ -1,7 +1,13 @@
 import mongoose from 'mongoose'
 
 const patientSchema = new mongoose.Schema({
-    
+    name: {type: String, required: true},
+    address: {type: String, required: true},
+    diagonosedWith: {type: String, required: true},
+    age: {type: Number, required: true},
+    gender: {type: String,enum: ["M", "F", "O"], required: true},
+    admittedIn: {type: mongoose.Schema.Type.ObjectId, required: true},
+    bloodGroup: {type: String, required: true}
 }, {timestamps: true});
 
 export const Patient = mongoose.model('Patient', patientSchema);
