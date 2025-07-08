6f15b7938338549b273ec4c83949a06ac7b80ffc
Sahil Raturi <sahilraturi687@gmail.com>
2025-07-08 15:17:58 +0530

    Add subscription model

---
+++ L6/src/models/subscription.model.js
@@ -0,0 +1,15 @@
+import mongoose, { Schema } from "mongoose";
+
+
+const subscriptionScehma = new Schema({
+    subscribers: {
+        type: Schema.Types.ObjectId, // one who is subscribing
+        ref: "User"
+    },
+    channels: {
+        type: Schema.Types.ObjectId, // one to whom users are subscribing. In a way a channel is also a user. 
+        ref: "User"
+    }
+}, { timestamps: true });
+
+export const Subscription = mongoose.model("Subscription", subscriptionScehma);
